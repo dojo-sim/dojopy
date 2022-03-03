aa0e804fb6c4f7085d06d615377bf6f6d4ca2407
thowell <thowell@stanford.edu>
2022-03-02 23:06:18 -0800

    jax

---
+++ dojopy/jax.py
@@ -0,0 +1,25 @@
+from julia import Dojo as dojo
+import jax
+import jax.numpy as jnp
+from jax.config import config
+config.update("jax_enable_x64", True)
+
+@partial(jax.custom_vjp, nondiff_argnums=(0,))
+def dynamics(env, state, input):
+	dojo.step(env, state, input, diff=True)
+	return env.state 
+
+def dynamics_fwd(env, state, input):
+	next_state = dynamics(env, state, input)
+	jacobian_state = env.dynamics_jacobian_state 
+	jacobian_input = env.dynamics_jacobian_input
+	return next_state, (jacobian_state, jacobian_input)
+
+def dynamics_bwd(env, res, g):
+	jacobian_state, jacobian_input = res
+	return (jacobian_state.T @ g, jacobian_input.T @ g)
+
+dynamics.defvjp(dynamics_fwd, dynamics_bwd)
+
+
+
